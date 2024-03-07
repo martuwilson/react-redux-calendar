3afc80c86fdb1fca88354c723638fe9c21c98d7c
Martin Ezequiel Williner <111790529+martuwilson@users.noreply.github.com>
2024-03-06 21:29:00 -0300

    Proyect seed ready to begin

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import './styles.css'
+import { Calendar } from './Calendar.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Calendar />
   </React.StrictMode>,
 )
